7a67000e2214e85bbfa4ce23cbe335949fe98afd
jhonpraxedesumg <jsalazarz@miumg.edu.gt>
2024-02-10 08:59:05 -0600

    arreglos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 
 import org.example.clase.Persona;
+import org.example.clase.TablaPronostico;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,18 +16,22 @@ public class Main {
 //        for (Persona persona: personas){
 //            System.out.println(persona);
 //        }
-        //ahora con matrices
-        Persona[][]matrix=new Persona[2][2];
+                                                       //ahora con matrices
+//        Persona[][]matrix=new Persona[2][2];
+//
+//        matrix[0][0]=new Persona("Juan   ",20);
+//        matrix[0][1]=new Persona("Pedro   ",21);
+//        matrix[1][0]=new Persona("Jose   ",27);
+//        matrix[1][1]=new Persona("Lucia  ",28);
+//
+//        for (Persona[] fila: matrix){
+//            for (Persona persona : fila){
+//                System.out.println(persona);
+//            }
 
-        matrix[0][0]=new Persona("Juan   ",20);
-        matrix[0][1]=new Persona("Pedro   ",21);
-        matrix[1][0]=new Persona("Jose   ",27);
-        matrix[1][1]=new Persona("Lucia  ",28);
+        //TABLA PRONOSTICO
 
-        for (Persona[] fila: matrix){
-            for (Persona persona : fila){
-                System.out.println(persona);
-            }
-        }
+      //  }
+        TablaPronostico tabla = new
     }
 }
--- src/main/java/org/example/clase/TablaPronostico.java
+++ src/main/java/org/example/clase/TablaPronostico.java
@@ -1,12 +1,21 @@
 package org.example.clase;
 
 import java.util.Arrays;
+import java.util.stream.DoubleStream;
 
 public class TablaPronostico {
     public void calculos() {
         double[] demanda = {133, 292, 283, 302, 400, 505, 608, 667, 783, 785, 799};
         double[] periodo = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
         double pronostico = calcularPronostico(demanda,periodo,10);
+        for( int x=0;x<periodo.length;x++){
+            pronostico = calcularPronostico(demanda,periodo,x);
+            System.out.println(x+ " .pronostico= "+pronostico);
+        }
+        DoubleStream StreamPeriodo = DoubleStream.of(periodo);
+        StreamPeriodo.forEach((mes))->{
+            System.out.println("mes= "+ mes+ );
+        }
     }
     private double calcularPronostico(double[] y, double[] x, double valorX){
         double n= y.length;
